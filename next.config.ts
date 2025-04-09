ffb214f97ad533c74f339d646d9da7b25a622220
Amirreza Hemmati <official.hemati@gmail.com>
2025-04-09 23:50:46 +0330

    fix pdf issue

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig: NextConfig = {
+  serverExternalPackages: ["playwright-core"],
+};
 
 export default nextConfig;
