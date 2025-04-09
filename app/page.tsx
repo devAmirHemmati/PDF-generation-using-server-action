63089222fca30258a1b7d2676ea46cdca4b8079c
Amirreza Hemmati <official.hemati@gmail.com>
2025-04-09 13:21:34 +0330

    feat

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,73 @@
-import Image from "next/image";
+"use client";
+import { generatePDF } from "@/serverActions/generatePdf";
+import { AllHTMLAttributes, useRef, useState } from "react";
 
-export default function Home() {
+function Button({
+  type = "button",
+  className = "",
+  children,
+  ...props
+}: AllHTMLAttributes<HTMLButtonElement>) {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <button
+      type={type as never}
+      className={`${className} px-6 py-3 bg-sky-700 text-white font-bold cursor-pointer rounded disabled:cursor-default disabled:opacity-60`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+function PdfDocument() {
+  return (
+    <div>
+      <h1 className="text-lg font-bold">Hello my pdf</h1>
+    </div>
+  );
+}
+
+export default function HomePage() {
+  const pdfRef = useRef<HTMLDivElement>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  async function onGenPdfClick() {
+    if (!pdfRef.current) return;
+
+    setLoading(true);
+
+    try {
+      const buffer = await generatePDF(pdfRef.current.innerHTML);
+
+      if (buffer === null) return;
+
+      const blob = new Blob([buffer]);
+      const url = URL.createObjectURL(blob);
+
+      const link = document.createElement("a");
+      link.download = "gen.pdf";
+      link.href = url;
+      link.click();
+
+      URL.revokeObjectURL(url);
+    } catch (error) {
+      console.log("ERROR: ", error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <div className="w-screen h-screen flex items-center justify-center flex-col gap-3">
+      <div ref={pdfRef} className="hidden">
+        <PdfDocument />
+      </div>
+
+      <h1 className="text-4xl border-b pb-2">Version 1.0.0</h1>
+
+      <Button disabled={loading} onClick={onGenPdfClick}>
+        Get PDF
+      </Button>
     </div>
   );
 }
---
+++ serverActions/generatePdf.ts
@@ -0,0 +1,52 @@
+"use server";
+import { Browser } from "puppeteer-core";
+
+export async function generatePDF(html: string): Promise<Buffer | null> {
+  // Initiate the browser instance
+  let browser: Browser | undefined | null;
+
+  // Check if the environment is development
+  if (process.env.NODE_ENV !== "development") {
+    // Import the packages required on production
+    const chromium = require("@sparticuz/chromium");
+    const puppeteer = require("puppeteer-core");
+
+    // Assign the browser instance
+    browser = await puppeteer.launch({
+      executablePath: await chromium.executablePath(),
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+      defaultViewport: chromium.defaultViewport,
+      args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+    });
+  } else {
+    // Else, use the full version of puppeteer
+    const puppeteer = require("puppeteer");
+    browser = await puppeteer.launch({
+      headless: true,
+    });
+  }
+
+  // Create a PDF
+  if (browser) {
+    const page = await browser.newPage();
+    await page.setContent(html);
+
+    const pdfBuffer = await page.pdf({
+      format: "a4",
+      printBackground: true,
+      margin: {
+        top: 80,
+        bottom: 80,
+        left: 80,
+        right: 80,
+      },
+    });
+
+    await browser.close();
+
+    return pdfBuffer;
+  }
+
+  return null;
+}
