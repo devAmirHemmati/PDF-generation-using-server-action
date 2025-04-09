7059afd6ae58cf14eb1871ec835097ebea3fa143
Amirreza Hemmati <official.hemati@gmail.com>
2025-04-09 15:13:01 +0330

    feat

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  serverExternalPackages: ["puppeteer-core", "chrome-aws-lambda"],
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
